103054e92f98977ca88cdf9aab378fefed9c7ef9
Devendra0230 <devendralodhi945@gmail.com>
2024-09-10 11:45:25 +0530

    4th

--- assistent/myapp/views.py
+++ assistent/myapp/views.py
@@ -21,8 +21,10 @@ logger = logging.getLogger(__name__)
 
 current_language = 'en-IN'  # Default to Indian English
 
+
 def index(request):
     return render(request, 'index.html')
+
 
 @csrf_exempt
 def text_to_speech(request):
@@ -31,28 +33,47 @@ def text_to_speech(request):
         text = data.get('text', '')
         try:
             tts = gTTS(text, lang=current_language)
-            
+
             # Generate a unique filename using the current timestamp
             timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
             audio_filename = f"audio_{timestamp}.mp3"
-            audio_path = os.path.join(settings.BASE_DIR, 'static/audio', audio_filename)
-            
+            try:
+                old_filepath = os.path.join(
+                    settings.BASE_DIR, 'static/tts_audio')
+                files = os.listdir(old_filepath)
+                for file in files:
+                    os.remove(f'{old_filepath}/{file}')
+            except:
+                ...
+            audio_path = os.path.join(
+                settings.BASE_DIR, 'static/tts_audio', audio_filename)
+
             tts.save(audio_path)
-            audio_url = settings.STATIC_URL + f'audio/{audio_filename}'
-            
+            audio_url = settings.STATIC_URL + f'tts_audio/{audio_filename}'
+
             return JsonResponse({'status': 'Text-to-speech conversion successful', 'audio_url': audio_url})
         except Exception as e:
             logger.error(f"Error in text-to-speech conversion: {e}")
             return JsonResponse({'error': 'Text-to-speech conversion failed'})
     return JsonResponse({'error': 'Invalid request'})
 
-# handle_post_processing function
+# handle post processing function
+
+
 def handle_post_processing(response_text):
     try:
         tts = gTTS(text=response_text, lang=current_language)
         timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
         audio_filename = f"response_{timestamp}.mp3"
-        audio_path = os.path.join(settings.BASE_DIR, 'static/audio', audio_filename)
+        try:
+            old_filepath = os.path.join(settings.BASE_DIR, 'static/stt_audio')
+            files = os.listdir(old_filepath)
+            for file in files:
+                os.remove(f'{old_filepath}/{file}')
+        except:
+            ...
+        audio_path = os.path.join(
+            settings.BASE_DIR, 'static/stt_audio', audio_filename)
         tts.save(audio_path)
 
         return audio_filename
@@ -61,6 +82,7 @@ def handle_post_processing(response_text):
         logger.error(f"Error during post-processing: {e}")
         return None
 
+
 @csrf_exempt
 def speech_to_text(request):
     try:
@@ -68,7 +90,7 @@ def speech_to_text(request):
             recognizer = sr.Recognizer()
             with sr.Microphone() as source:
                 recognizer.adjust_for_ambient_noise(source)
-                audio = recognizer.listen(source, timeout=5, phrase_time_limit=10)
+                audio = recognizer.listen(source)
 
             query_text = recognizer.recognize_google(audio)
             response_text = send_to_meta_ai(query_text)
@@ -77,7 +99,7 @@ def speech_to_text(request):
             audio_filename = handle_post_processing(response)
 
             if audio_filename:
-                audio_url = settings.STATIC_URL + 'audio/' + audio_filename
+                audio_url = settings.STATIC_URL + 'stt_audio/' + audio_filename
                 return JsonResponse({
                     'status': 'success',
                     'query': query_text,
@@ -92,10 +114,11 @@ def speech_to_text(request):
         logger.error(f"Server error: {e}")
         return JsonResponse({'status': 'error', 'message': 'Internal server error'})
 
+
 def send_to_meta_ai(query_text):
     try:
         ai = MetaAI()
-        logger.debug(f"Sendj query to Meta AI: {query_text}")
+        logger.debug(f"Send query to Meta AI: {query_text}")
         response = ai.prompt(message=query_text)
         logger.debug(f"response from Meta AI: {response}")
         return response
